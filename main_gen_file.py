73245cd094e2b9f69d80f146d94e81c47645d664
mgSeo <mgseo1218@gmail.com>
2022-06-01 00:39:22 +0900

    Add: 시계열 데이터 생성 main.py

---
+++ main_gen_file.py
@@ -0,0 +1,43 @@
+# import numpy as np
+import pandas as pd
+
+Foldernames = ['10015_20220417-20220517', 'VABJ0052_20220417-20220517', 'VABJ0079_20220417-20220517']
+cars = ['10015', 'VABJ0052', 'VABJ0079']
+Path = 'Result_accumulated/'
+for car in range(3):
+    ## parameters ############################
+    Foldername = Foldernames[car]
+    Carname = cars[car]
+    ## pre-set ############################    
+    Start_day = Foldername[-17:-9]
+    End_day = Foldername[-8:]
+    dt_index = pd.date_range(start=Start_day, end=End_day)
+    dt_list = dt_index.strftime("%Y%m%d").tolist()
+    index = ['clientid','timestamp','cell1','cell2','cell3','cell4','cell_cap',\
+                'current1','current2','current3','current4','etc','port1','port2','rport','saac','sac','soc','state',\
+                    'temperature1','temperature2','temperature3','temperature4','temperature5','temperature6','timeindex','version','volt','wport']
+    ## pre-set ############################
+
+
+    df = pd.DataFrame() # df: 15분 데이터 적층용 데이터프레임
+    for day in range(len(dt_list)):
+        # if day != 3:
+        #     continue
+        Filename = 'Datas/' + Foldername + '/' + Carname + '-bms_records-' + dt_list[day] + '.csv'    
+        Data = pd.read_csv(Filename)
+        
+        ### 데이터 전처리
+        import Preprocess
+        data = Preprocess.func(Data,index)
+
+        ### load 분리
+        import Disaggregator
+        data,r_index = Disaggregator.func(data,index)
+
+        ### load, PV 시계열로 출력
+        import Combine_15min as C15
+        data15 = C15.func(data,r_index)
+
+        ### 적층적층
+        df = pd.concat([df,data15])
+    df.to_csv(Path + Carname+'_Accumulated_15min_datas.csv')
